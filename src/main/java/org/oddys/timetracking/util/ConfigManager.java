7b6618acd5ada9d488650ac9ae1e9ceac3820120
Oddys <yuriy.honcharenko@gmail.com>
2019-09-12 10:18:12 +0300

    Refactor ConfigManeger to include sql_queries.properties

--- src/main/java/org/oddys/timetracking/util/ConfigManager.java
+++ src/main/java/org/oddys/timetracking/util/ConfigManager.java
@@ -11,16 +11,18 @@ public class ConfigManager {
     public static final String CABINET_PATH = "path.cabinet";
     public static final String USERS_LIST_PATH = "path.users.list";
     private static final String CONFIG_FILE_NAME = "config.properties";
+    private static final String SQL_QUERIES_FILE_NAME = "db/sql_queries.properties";
     private final Properties PROPERTIES;
 
     private ConfigManager() {
-        InputStream inputStream = ConfigManager.class.getClassLoader()
-                .getResourceAsStream(CONFIG_FILE_NAME);
         Properties properties = new Properties();
-        try {
-            properties.load(inputStream);
+        ClassLoader classLoader = ConfigManager.class.getClassLoader();
+        try (InputStream config = classLoader.getResourceAsStream(CONFIG_FILE_NAME);
+             InputStream sqlQueries = classLoader.getResourceAsStream(SQL_QUERIES_FILE_NAME)) {
+            properties.load(config);
+            properties.load(sqlQueries);
         } catch (IOException | NullPointerException | IllegalArgumentException e) {
-            throw new ResourceInitializationException("Failed to obtain configuration properties from a file", e);
+            throw new ResourceInitializationException("Failed to obtain properties from a file", e);
         }
         PROPERTIES = properties;
     }
