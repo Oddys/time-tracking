f2d7f89e1f1f4d11824fe9a62af73064aaadb68a
Oddys <yuriy.honcharenko@gmail.com>
2019-10-07 18:47:16 +0300

    Fix ShowUserActivitiesCommand

--- src/main/java/org/oddys/timetracking/command/ShowUserActivitiesCommand.java
+++ src/main/java/org/oddys/timetracking/command/ShowUserActivitiesCommand.java
@@ -9,18 +9,32 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 public class ShowUserActivitiesCommand implements Command {
-    private static final ShowUserActivitiesCommand INSTANCE = new ShowUserActivitiesCommand();
+    private static final Command INSTANCE = new ShowUserActivitiesCommand();
     private SearchUserActivitiesService searchService = SearchUserActivitiesServiceImpl.getInstance();
 
     private ShowUserActivitiesCommand() {}
 
-    public static ShowUserActivitiesCommand getInstance() {
+    public static Command getInstance() {
         return INSTANCE;
     }
 
+//    @Override
+//    public String execute(HttpServletRequest req) {
+//        String userIdString = req.getParameter("userId");
+//        if (userIdString == null) {
+//            return ConfigManager.getInstance().getProperty("path.home");
+//        }
+//        Long userId = Long.valueOf(userIdString);
+//        List<UserActivityDto> userActivityDTOs = searchService.searchUserActivitiesByUserId(userId);
+//        req.getSession().setAttribute("userFirstName", req.getParameter("userFirstName"));
+//        req.getSession().setAttribute("userLastName", req.getParameter("userLastName"));
+//        req.getSession().setAttribute("userActivities", userActivityDTOs);
+//        return ConfigManager.getInstance().getProperty("path.user.activities");
+//    }
+
     @Override
     public String execute(HttpServletRequest req) {
-        String userIdString = req.getParameter("userId");
+        String userIdString = req.getParameter("userId");  // TODO Add validation
         if (userIdString == null) {
             return ConfigManager.getInstance().getProperty("path.home");
         }
@@ -29,6 +43,6 @@ public class ShowUserActivitiesCommand implements Command {
         req.getSession().setAttribute("userFirstName", req.getParameter("userFirstName"));
         req.getSession().setAttribute("userLastName", req.getParameter("userLastName"));
         req.getSession().setAttribute("userActivities", userActivityDTOs);
-        return ConfigManager.getInstance().getProperty("path.user.activities");
+        return "/user-activities";
     }
 }
