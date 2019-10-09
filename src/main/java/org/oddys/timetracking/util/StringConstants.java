3f3ebddee012c63d0f065f899f5590ae23cf28bc
Oddys <yuriy.honcharenko@gmail.com>
2019-10-09 15:45:32 +0300

    Remove unused StringConstants

--- src/main/java/org/oddys/timetracking/util/StringConstants.java
+++
@@ -1,7 +0,0 @@
-package org.oddys.timetracking.util;
-
-public class StringConstants {
-    public static final String REDIRECT = "redirect:";
-    public static final String EMPTY_STRING = "";
-    public static final String COMMAND = "command";
-}
